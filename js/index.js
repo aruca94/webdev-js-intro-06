dee282034401b5cf899de2a55b04c01b453991a7
Josh <aftershock840@gmail.com>
2024-12-28 11:55:36 -0500

    added loops to file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+// "use strict"
+// //for loop with no break
+// console.log(`first loop start`);
+// for( let i = 5; i <= 10; i++){
+//     console.log(i);
+// };
+// console.log(`first loop end`);
+//for loop with break
+// console.log(`second loop start`);
+// for( let i = 0; i <= 5; i++){
+//     console.log(i);
+//         if( i === 3){
+//             break;
+//         };
+// };
+// console.log(`second loop end`);
+// // for loop with continue
+// console.log(`third loop start`);
+// for( let i = 20; i <= 35; i++){
+//     if( i % 2 === 0){
+//         console.log(`${i} is even`)
+//     }else if(i % 2 === 1){
+//         console.log(`${i} is odd`);
+//     };
+// };
+// console.log(`third loop end`);
